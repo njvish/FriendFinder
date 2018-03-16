df60f2b1aee1e49d5c36ee541cc34182d3e40232
Nicholas Vicchrilli <nvicchrilli@nvicchrilli-l.corp.instructure.com.corp.instructure.com>
2018-03-15 22:40:32 -0600

    Big changes and redo

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+        name: "Star Lord",
+        photo: "http://marvelcinematicuniverse.wikia.com/wiki/Star-Lord?file=StarLord-Guardians2-Profile.jpg",
+        scores: [
+            "5",
+            "4",
+            "1",
+            "1",
+            "2",
+            "5",
+            "3",
+            "2",
+            "4",
+            "3",
+        ]
+    },
+    {
+        name: "Drax",
+        photo: "missing",
+        scores: [
+            "4",
+            "3",
+            "5",
+            "5",
+            "1",
+            "4",
+            "2",
+            "1",
+            "3",
+            "2",
+        ]
+    },
+    {
+        name: "Gamora",
+        photo: "http://marvelcinematicuniverse.wikia.com/wiki/Star-Lord?file=StarLord-Guardians2-Profile.jpg",
+        scores: [
+            "1",
+            "5",
+            "2",
+            "2",
+            "3",
+            "1",
+            "4",
+            "3",
+            "5",
+            "4",
+        ]
+    },
+    {
+        name: "Rocket",
+        photo: "missing",
+        scores: [
+            "5",
+            "3",
+            "2",
+            "2",
+            "3",
+            "4",
+            "4",
+            "4",
+            "5",
+            "4",
+        ]
+    },
+    {
+        name: "Groot",
+        photo: "missing",
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3",
+            "3",
+            "4",
+        ]
+    }
+];
+
+module.exports = friends;
